6841d1310ff656a39cf8a8a2d63948f89a3ed2ea
ewa0601 <egmu31@gmail.com>
2020-06-19 14:53:38 +0200

    Library resistant to errors

--- src/pl/javastart/library/app/LibraryApp.java
+++ src/pl/javastart/library/app/LibraryApp.java
@@ -2,7 +2,7 @@ package pl.javastart.library.app;
 
 class LibraryApp {
 
-    private final static String APP_NAME = "Biblioteka v1.5";
+    private final static String APP_NAME = "Biblioteka v1.6";
 
     public static void main(String[] args) {
         System.out.println(APP_NAME);
@@ -16,5 +16,6 @@ class LibraryApp {
 //        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
 //                "Bert Bates, Katherine Sierra", 2008, 851,
 //                "McGraw-Hill Osborne Media");
+//        System.out.println("System może przechowywać do " + books.length + " książek");
     }
 }
