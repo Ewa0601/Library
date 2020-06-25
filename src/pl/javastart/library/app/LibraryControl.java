79943d670e253ac4f8c975cfe1b3ca311d9d25b9
ewa0601 <egmu31@gmail.com>
2020-06-25 11:57:03 +0200

    Option class as inner class in LibraryControl

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -108,4 +108,34 @@ public class LibraryControl {
         dataReader.close();
     }
 
+
+    private enum Option {
+        EXIT(0, "Wyjście z programu"),
+        ADD_BOOK(1, "Dodanie nowej książki"),
+        ADD_MAGAZINE(2, "Dodanie nowego magazynu/gazety"),
+        PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+        PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów/gazet");
+
+        private final int value;
+        private final String description;
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        static Option createFromInt(int option) throws NoSuchOptionException {
+            try {
+                return Option.values()[option];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchOptionException("Brak opcji o id: " + option);
+            }
+        }
+    }
+
 }
