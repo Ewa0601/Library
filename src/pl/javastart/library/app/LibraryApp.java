56fd903dfea4c528fd03a3a8cdbc1b08a3b44b8f
ewa0601 <egmu31@gmail.com>
2020-10-21 20:39:49 +0200

    Replace loops with streams in ConsolePrinter

--- src/pl/javastart/library/app/LibraryApp.java
+++ src/pl/javastart/library/app/LibraryApp.java
@@ -2,7 +2,7 @@ package pl.javastart.library.app;
 
 class LibraryApp {
 
-    private final static String APP_NAME = "Biblioteka v2.5";
+    private final static String APP_NAME = "Biblioteka v2.6";
 
     public static void main(String[] args) {
         System.out.println(APP_NAME);
